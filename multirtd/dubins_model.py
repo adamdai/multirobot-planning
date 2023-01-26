0f0ce3d2a514d12b6f9c632e96486520dfb8e580
Adam Dai <adamdai97@gmail.com>
2023-01-26 13:41:42 -0800

    updating system id notebook

---
+++ multirtd/dubins_model.py
@@ -0,0 +1,55 @@
+"""Dubins dynamics model
+
+"""
+
+import numpy as np
+
+
+def dubins_step(x, u, dt):
+    """Run one step of dynamics
+
+    Parameters
+    ----------
+    x : np.array
+        State vector (x, y, theta)
+    u : np.array
+        Control vector (v, w)
+    dt : float
+        Time step
+
+    Returns
+    -------
+    np.array
+        Updated state vector (x, y, theta)
+
+    """
+    x_dot = u[0] * np.cos(x[2])
+    y_dot = u[0] * np.sin(x[2])
+    theta_dot = u[1]
+    x_new = x + np.array([x_dot, y_dot, theta_dot]) * dt
+    return x_new
+
+
+def dubins_traj(x0, U, dt):
+    """Compute dubins trajectory from a sequence of controls
+    
+    Parameters
+    ----------
+    x0 : np.array
+        Initial state vector (x, y, theta)
+    U : np.array
+        Control sequence (v, w)
+    dt : float
+        Time step
+    
+    Returns
+    -------
+    np.array
+        Trajectory (x, y, theta)
+    
+    """
+    traj = np.zeros((len(U), 3))
+    traj[0] = x0
+    for i in range(1, len(U)):
+        traj[i] = dubins_step(traj[i-1], U[i-1], dt)
+    return traj
