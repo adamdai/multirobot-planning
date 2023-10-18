24bce49e218d5a3aa9a469d0ae330054ba150d56
Adam Dai <adamdai97@gmail.com>
2023-10-18 12:02:54 -0700

    closed form dubins dynamics

--- multirtd/dynamics/dubins_model.py
+++ multirtd/dynamics/dubins_model.py
@@ -82,6 +82,20 @@ def dubins_traj(x0, u, N, dt):
     return traj
 
 
+def closed_form_dubins_traj(x0, u, N, dt):
+    def dx(v, w, t, eps=1e-15):
+        return v * np.sin((w + eps) * t) / (w + eps)
+    def dy(v, w, t, eps=1e-15):
+        return v * (1 - np.cos((w + eps) * t)) / (w + eps)
+
+    traj = np.zeros((N, 3))
+    traj[:,0] = [x0[0] + dx(u[0], u[1], i * dt) for i in range(N)]
+    traj[:,1] = [x0[1] + dy(u[0], u[1], i * dt) for i in range(N)]
+    traj[:,2] = x0[2] + u[1] * np.arange(N) * dt
+
+    return traj
+
+
 def dubins_controls(u_0, u_pk):
     """Compute controls for dubins trajectory
     
